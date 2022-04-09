e064dfbdcc315dc07f103b943d8527c6698173fb
joel-CM <cjoel1977@gmail.com>
2022-04-09 11:51:11 -0300

    add contact url

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import style from "./Footer.module.css";
+import Link from "next/link";
 // icons
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
@@ -7,8 +8,12 @@ export default function Footer() {
     <div className={style.container}>
       <h3 className={style.containerTitle}>CONTACT</h3>
       <div className={style.containerIcons}>
-        <AiFillGithub className={style.icon} />
-        <AiFillLinkedin className={style.icon} />
+        <Link href={"https://github.com/joel-CM"}>
+          <AiFillGithub className={style.icon} />
+        </Link>
+        <Link href={"https://www.linkedin.com/in/joel-c-dev/"}>
+          <AiFillLinkedin className={style.icon} />
+        </Link>
       </div>
     </div>
   );
